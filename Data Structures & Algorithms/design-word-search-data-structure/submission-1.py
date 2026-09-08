e0ace0ce77dd49dc139bf97b345bb8036baf0edf
Jonah Escobar <jonahe2006@gmail.com>
2026-09-08 16:57:09 -0700

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Node()
+            curr = curr.children[c]
+        curr.isWord = True
+
+    def search(self, word: str) -> bool:
+        def dfs(index, root):
+            curr = root
+            for i in range(index, len(word)):
+                c = word[i]
+                if c == ".":
+                    for val in curr.children.values():
+                        if dfs(i + 1, val):
+                            return True
+                    return False
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+            return curr.isWord
+        return dfs(0, self.root)
